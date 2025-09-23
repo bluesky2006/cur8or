543f8d9084b80c761875bd0e22ba0499291774b9
bluesky2006 <simon.busby@gmail.com>
2025-09-23 13:12:25 +0100

    redo api

--- lib/api/cma.ts
+++ lib/api/cma.ts
@@ -1,16 +1,5 @@
 const BASE_URL = "https://openaccess-api.clevelandart.org/api";
 
-export async function cmaSearchArtworks(query: string, limit = 10) {
-  const url = `${BASE_URL}/artworks?limit=${limit}&q=${encodeURIComponent(query)}`;
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error(`API error: ${res.status}`);
-  }
-
-  return res.json();
-}
-
 export async function cmaGetArtworkById(id: number) {
   const url = `${BASE_URL}/artworks/${id}`;
   const res = await fetch(url);
@@ -22,10 +11,21 @@ export async function cmaGetArtworkById(id: number) {
   return res.json();
 }
 
-// cmaSearchArtworks("monet", 5)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
+export async function cmaSearchArtworks(query: string, limit = 10) {
+  const url = `${BASE_URL}/artworks?limit=${limit}&q=${query}`;
+  const res = await fetch(url);
 
-// cmaGetArtworkById(125234)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
+  if (!res.ok) {
+    throw new Error(`API error: ${res.status}`);
+  }
+
+  return res.json();
+}
+
+// cmaGetArtworkById(130707)
+//   .then((artwork) => console.log(artwork, "<artwork"))
+//   .catch((error) => console.error(error, "Error yo"));
+
+// cmaSearchArtworks("song xu", 5)
+//   .then((response) => console.log(response, "<search response"))
+//   .catch((error) => console.error(error));
