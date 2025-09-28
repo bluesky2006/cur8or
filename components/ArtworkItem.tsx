55e6ee1033b2dff5c9363bcfb04586d89e982a27
bluesky2006 <simon.busby@gmail.com>
2025-09-28 14:52:41 +0100

    update styling to truncate titles to 2 lines

--- components/ArtworkItem.tsx
+++ components/ArtworkItem.tsx
@@ -4,7 +4,7 @@ import type { ArtworkItemProps } from "../types/types";
 
 export default function ArtworkItem({ art }: ArtworkItemProps) {
   return (
-    <div className="p-4 border rounded-lg shadow-sm bg-white flex flex-col">
+    <div className="p-4  rounded-lg shadow-xl bg-white flex flex-col">
       <div className="w-full aspect-square relative bg-gray-200 rounded overflow-hidden">
         {art.images?.web?.url ? (
           <Image src={art.images.web.url} alt={art.title} fill className="object-cover" />
@@ -14,8 +14,7 @@ export default function ArtworkItem({ art }: ArtworkItemProps) {
           </div>
         )}
       </div>
-
-      <h3 className="mt-2 font-semibold">{art.title}</h3>
+      <h3 className="mt-2 font-semibold line-clamp-2">{art.title}</h3>
       <p className="text-sm text-gray-600">{art.creators.map((c) => c.description).join(", ")}</p>
     </div>
   );
