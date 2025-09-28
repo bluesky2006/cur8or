e2b1c56fc238487252331e49f545ec3a98de4429
bluesky2006 <simon.busby@gmail.com>
2025-09-28 14:57:45 +0100

    update styling

--- components/ArtworkItem.tsx
+++ components/ArtworkItem.tsx
@@ -1,20 +1,19 @@
-// components/ArtworkItem.tsx
 import Image from "next/image";
 import type { ArtworkItemProps } from "../types/types";
 
 export default function ArtworkItem({ art }: ArtworkItemProps) {
   return (
     <div className="p-4  rounded-lg shadow-xl bg-white flex flex-col">
-      <div className="w-full aspect-square relative bg-gray-200 rounded overflow-hidden">
+      <div className="w-full aspect-square relative bg-yellow-100 rounded overflow-hidden">
         {art.images?.web?.url ? (
           <Image src={art.images.web.url} alt={art.title} fill className="object-cover" />
         ) : (
-          <div className="w-full h-full flex items-center justify-center text-gray-400 text-sm">
+          <div className="w-full h-full flex items-center justify-center text-gray-900 text-sm">
             No image
           </div>
         )}
       </div>
-      <h3 className="mt-2 font-semibold line-clamp-2">{art.title}</h3>
+      <h3 className="mt-2 font-semibold line-clamp-2 leading-tight">{art.title}</h3>
       <p className="text-sm text-gray-600">{art.creators.map((c) => c.description).join(", ")}</p>
     </div>
   );
