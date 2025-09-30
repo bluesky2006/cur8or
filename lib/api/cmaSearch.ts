873fb202e60dcc4d3a89608580dd4df6600c5aa8
bluesky2006 <simon.busby@gmail.com>
2025-09-30 10:59:25 +0100

    rename cma to cmaSearch

--- lib/api/cmaSearch.ts
+++ lib/api/cmaSearch.ts
@@ -1,8 +1,8 @@
-import { Artwork } from "../../types/types";
+import { CmaArtwork } from "../../types/artTypes";
 
 const BASE_URL = "https://openaccess-api.clevelandart.org/api";
 
-export async function cmaSearchArtworks(query: string, skip = 0, limit = 9): Promise<Artwork[]> {
+export async function cmaSearchArtworks(query: string, skip = 0, limit = 9): Promise<CmaArtwork[]> {
   const res = await fetch(
     `${BASE_URL}/artworks?limit=${limit}&skip=${skip}&q=${encodeURIComponent(query)}`
   );
@@ -12,5 +12,5 @@ export async function cmaSearchArtworks(query: string, skip = 0, limit = 9): Pro
     throw new Error(`API error: ${res.status}`);
   }
 
-  return (Array.isArray(json.data) ? json.data : []) as Artwork[];
+  return (Array.isArray(json.data) ? json.data : []) as CmaArtwork[];
 }
