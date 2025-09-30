dcdd16e18b6f73babbeecd3fa4ce860ff260de61
bluesky2006 <simon.busby@gmail.com>
2025-09-30 10:58:05 +0100

    display more info

--- components/ArtworkItem.tsx
+++ components/ArtworkItem.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import type { ArtworkItemProps } from "../types/types";
+import type { ArtworkItemProps } from "../types/artTypes";
 
 export default function ArtworkItem({ art }: ArtworkItemProps) {
+  console.log("ArtworkItem", art);
   return (
     <div className="p-4  rounded-lg shadow-xl bg-white flex flex-col">
       <div className="w-full aspect-square relative bg-yellow-100 rounded overflow-hidden">
-        {art.images?.web?.url ? (
-          <Image src={art.images.web.url} alt={art.title} fill className="object-cover" />
+        {art.imageUrl ? (
+          <Image src={art.imageUrl} alt={art.title} fill className="object-cover" />
         ) : (
           <div className="w-full h-full flex items-center justify-center text-gray-900 text-sm">
             No image
@@ -14,7 +15,11 @@ export default function ArtworkItem({ art }: ArtworkItemProps) {
         )}
       </div>
       <h3 className="mt-2 font-semibold line-clamp-2 leading-tight">{art.title}</h3>
-      <p className="text-sm text-gray-600">{art.creators.map((c) => c.description).join(", ")}</p>
+      <p className="text-sm text-gray-600">{art.artist}</p>
+      <p className="text-sm text-gray-600">{art.date}</p>
+      <p className="text-sm text-gray-600">
+        Source: {art.source === "cma" ? "Cleveland Art Museum" : "Smithsonian Institute"}
+      </p>
     </div>
   );
 }
