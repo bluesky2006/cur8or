d4e0b17602e5c50f51c300d8b531508ac4b534a8
bluesky2006 <simon.busby@gmail.com>
2025-10-01 16:15:29 +0100

    update Artworkltem card to display ArtworkDetailModal

--- components/ArtworkItem.tsx
+++ components/ArtworkItem.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image";
 import type { ArtworkItemProps } from "../types/artTypes";
+import ArtworkDetailModal from "./ArtworkDetailModal";
+import { useState } from "react";
 
 export default function ArtworkItem({ art }: ArtworkItemProps) {
-  console.log("ArtworkItem", art);
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="p-4  rounded-lg shadow-xl bg-white flex flex-col">
-      <div className="w-full aspect-square relative bg-yellow-100 rounded overflow-hidden">
-        {art.imageUrl ? (
-          <Image src={art.imageUrl} alt={art.title} fill className="object-cover" />
-        ) : (
-          <div className="w-full h-full flex items-center justify-center text-gray-900 text-sm">
-            No image
-          </div>
-        )}
+    <div>
+      <div
+        onClick={() => setShowModal(true)}
+        className="p-4 rounded-lg shadow-xl bg-white flex flex-col cursor-pointer hover:shadow-2xl transition"
+      >
+        <div className="w-full aspect-square relative bg-yellow-100 rounded overflow-hidden">
+          {art.imageUrl ? (
+            <Image src={art.imageUrl} alt={art.title} fill className="object-cover" />
+          ) : (
+            <div className="w-full h-full flex items-center justify-center text-gray-900 text-sm">
+              No image
+            </div>
+          )}
+        </div>
+        <h3 className="mt-2 font-semibold line-clamp-2 leading-tight">{art.title}</h3>
+        <p className="text-sm text-gray-600">{art.artist}</p>
+        <p className="text-sm text-gray-600">{art.date}</p>
+        <p className="text-sm text-gray-600">
+          Source: {art.source === "cma" ? "Cleveland Art Museum" : "Smithsonian Institute"}
+        </p>
       </div>
-      <h3 className="mt-2 font-semibold line-clamp-2 leading-tight">{art.title}</h3>
-      <p className="text-sm text-gray-600">{art.artist}</p>
-      <p className="text-sm text-gray-600">{art.date}</p>
-      <p className="text-sm text-gray-600">
-        Source: {art.source === "cma" ? "Cleveland Art Museum" : "Smithsonian Institute"}
-      </p>
+      {showModal && <ArtworkDetailModal art={art} onClose={() => setShowModal(false)} />}
     </div>
   );
 }
