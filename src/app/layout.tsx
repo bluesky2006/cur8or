48e4ea048bcadeec8f81f702b4822d4058b10150
bluesky2006 <simon.busby@gmail.com>
2025-10-06 15:21:27 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import "./globals.css";
 import { ExhibitionProvider } from "../../context/ExhibitionContext";
 
 export const metadata: Metadata = {
-  title: "cur8or",
-  description: "Exhibit your art",
+  title: "Cur8or",
+  description: "Curate your own virtual art exhibitions",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
