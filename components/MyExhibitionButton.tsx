61b065bec97d3349db972b3438077a1201d79abe
bluesky2006 <simon.busby@gmail.com>
2025-10-10 16:26:56 +0100

    tidy tailwind code

--- components/MyExhibitionButton.tsx
+++ components/MyExhibitionButton.tsx
@@ -6,8 +6,7 @@ export default function MyExhibitionButton({ exhibitionCount, onClick }: MyExhib
     <button
       type="button"
       onClick={onClick}
-      className="w-full sm:w-auto items-center justify-center flex bg-white text-amber-500 border border-amber-500 px-6 py-2 rounded 
-        hover:bg-amber-500 hover:text-white transition-colors cursor-pointer dark:bg-black"
+      className="w-full sm:w-auto items-center justify-center flex bg-white text-amber-500 border border-amber-500 px-6 py-2 rounded hover:bg-amber-500 hover:text-white transition-colors cursor-pointer dark:bg-black"
     >
       My Exhibition {exhibitionCount > 0 ? `(${exhibitionCount})` : null}
     </button>
