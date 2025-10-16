62f9ddd7a6e6e34b148fafa572adfb3238008179
bluesky2006 <simon.busby@gmail.com>
2025-10-16 20:48:31 +0100

    fix missing dark mode colour updates

--- components/MyExhibitionButton.tsx
+++ components/MyExhibitionButton.tsx
@@ -6,7 +6,7 @@ export default function MyExhibitionButton({ exhibitionCount, onClick }: MyExhib
     <button
       type="button"
       onClick={onClick}
-      className="w-full sm:w-auto items-center justify-center flex bg-[#f9f9f9] text-amber-500 border border-amber-500 px-6 py-2 rounded hover:bg-amber-500 hover:text-white transition-colors cursor-pointer dark:bg-black"
+      className="w-full sm:w-auto items-center justify-center flex bg-[#f9f9f9] text-amber-500 border border-amber-500 px-6 py-2 rounded hover:bg-amber-500 hover:text-white transition-colors cursor-pointer dark:bg-[#1a1a1a]"
     >
       My Exhibition {exhibitionCount > 0 ? `(${exhibitionCount})` : null}
     </button>
